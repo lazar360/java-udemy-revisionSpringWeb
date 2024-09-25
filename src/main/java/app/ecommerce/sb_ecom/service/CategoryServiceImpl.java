38ad520c331dac099c0db57028a9cb758c931357
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-25 14:31:16 +0200

    add update category

--- src/main/java/app/ecommerce/sb_ecom/service/CategoryServiceImpl.java
+++ src/main/java/app/ecommerce/sb_ecom/service/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package app.ecommerce.sb_ecom.service;
 
 import app.ecommerce.sb_ecom.model.Category;
+import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -8,9 +9,10 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
-public class CategoryServiceImpl implements CategoryService{
+public class CategoryServiceImpl implements CategoryService {
 
     private List<Category> categories = new ArrayList<>();
     private Long nextId = 1L;
@@ -28,9 +30,24 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public String deleteCategory(Long categoryId) {
-        if (this.categories.removeIf(category -> Objects.equals(category.getCategoryId(), categoryId))){
-        return "Category with categoryId " + categoryId + " deleted successfully";
+        if (this.categories.removeIf(category -> Objects.equals(category.getCategoryId(), categoryId))) {
+            return "Category with categoryId " + categoryId + " deleted successfully";
         }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found");
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> optionalCategory = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst();
+
+        if (optionalCategory.isPresent()){
+            Category existingCategory = optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found");
+        }
     }
 }
