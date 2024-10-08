e7ff0c830c2b48aa18c1a4608843b31572835af0
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-10-08 11:31:07 +0200

    Add 1 arg constructor to Category and create CategoryServiceImplTest

--- src/main/java/app/ecommerce/sb_ecom/model/Category.java
+++ src/main/java/app/ecommerce/sb_ecom/model/Category.java
@@ -6,19 +6,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity(name = "categories")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class Category {
     @Id()
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
+    @NonNull
     @NotBlank(message = "La catégorie ne doit pas être vide")
     @Size(min=5, message = "La catégorie ne doit pas avoir une taille inférieure à 5 caractères")
     private String categoryName;
---
+++ src/test/java/app/ecommerce/sb_ecom/CategoryServiceImplTest.java
@@ -0,0 +1,79 @@
+package app.ecommerce.sb_ecom;
+
+import app.ecommerce.sb_ecom.model.Category;
+import app.ecommerce.sb_ecom.repositories.CategoryRepository;
+import app.ecommerce.sb_ecom.service.CategoryServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+public class CategoryServiceImplTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private CategoryServiceImpl categoryService;
+
+    private Category category1;
+
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        category1 = new Category("category1");
+    }
+
+    @Test
+    public void shouldCreateCategory() {
+        categoryService.createCategory(category1);
+        when(categoryRepository.findAll()).thenReturn(List.of(category1));
+        assertEquals(1, categoryService.getAllCategories().size());
+    }
+
+    @Test
+    public void shouldGetAllCategories() {
+        when(categoryRepository.findAll()).thenReturn(List.of(category1));
+        assertEquals(1, categoryService.getAllCategories().size());
+    }
+
+    @Test
+    public void shouldUpdateCategory(){
+        // Arrange
+        when(categoryRepository.findById(category1.getCategoryId()))
+                .thenReturn(Optional.of(category1));
+        category1.setCategoryName("update");
+        when(categoryRepository.save(any(Category.class))).thenReturn(category1);
+
+        // Act
+        Category updatedCategory = categoryService.updateCategory(category1, category1.getCategoryId());
+
+        // Assert
+        assertEquals("update", updatedCategory.getCategoryName());
+    }
+
+    @Test
+    public void shouldDeleteCategory(){
+        // Arrange
+        category1.setCategoryId(1L); // Ensure the categoryId is set
+        when(categoryRepository.findById(category1.getCategoryId()))
+                .thenReturn(Optional.of(category1));
+
+        // Act
+        String result = categoryService.deleteCategory(category1.getCategoryId());
+
+        // Assert
+        verify(categoryRepository, times(1)).deleteById(category1.getCategoryId());
+        assertEquals("Category with categoryId: 1 deleted successfully", result);
+    }
+}
